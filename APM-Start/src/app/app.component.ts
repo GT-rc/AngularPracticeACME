32a5c53809b62d916dffdf41690a5bc86e753c86
Glynnis Thomas <glynnis.thomas@kdol.ks.gov>
2019-06-13 14:44:51 -0500

    Angular practice app push for wfh

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+            /* `<div>
+              <h1>{{ pageTitle }}</h1>
+              <div> My First Component!</div>
+              <pm-products></pm-products>
+            </div>` */
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
